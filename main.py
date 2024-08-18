8ccfedac6dc28da0aff87ffd276817e71687f85c
fountesh <128893715+fountesh@users.noreply.github.com>
2024-08-18 13:47:53 +0300

    update3

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import sqlite3
+
+conn = sqlite3.connect("colage.db")
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    major TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS courses(
+    course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    course_name TEXT NOT NULL, 
+    instructor TEXT NOT NULL
+)
+''')
